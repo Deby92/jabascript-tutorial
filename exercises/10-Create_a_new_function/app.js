f2bf4ca8ae942e985f59f820d136eb0f98a92c9f
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-06-22 22:10:03 +0000

    wednesday

--- exercises/10-Create_a_new_function/app.js
+++ exercises/10-Create_a_new_function/app.js
@@ -1,3 +1,9 @@
 /* This exercise is almost identical to 15-Random numbers - consider merging them here and using another concept for 15? */
 
 // Declare your function here
+function getRandomInt()
+{
+	var randomNumber = Math.random((* 10)+ 1);
+	return randomNumber;
+}
+console.log(getRandomInt());
