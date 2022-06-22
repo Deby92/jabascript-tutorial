faff8456d10ff045613f606f8e36252871fa2fad
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-06-22 04:40:20 +0000

    holiday

--- .learn/resets/09-Creating_your_first_function/app.js
+++ .learn/resets/09-Creating_your_first_function/app.js
@@ -1,6 +1,6 @@
 function addNumbers(a,b)
 {
-	return 3+4// This is the function body. Write your code here.
+	// This is the function body. Write your code here.
 }
 
 //Do not change the code below
