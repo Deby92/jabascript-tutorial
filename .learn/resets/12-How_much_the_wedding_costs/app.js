181ce1c43b518b894c437de54690e6d721d93cdf
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-06-23 23:45:57 +0000

    jueves

--- .learn/resets/12-How_much_the_wedding_costs/app.js
+++ .learn/resets/12-How_much_the_wedding_costs/app.js
@@ -3,9 +3,8 @@ let guests = prompt('How many people are coming to your wedding?');
 function getPrice(guests){
     let cost = 0;
     // Your code here
-    if(guests > 200)
     return cost;
 }
 
 let price = getPrice(guests);
-console.log('Your wedding will cost '+price+' dollars');   
+console.log('Your wedding will cost '+price+' dollars');
